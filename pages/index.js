544e838d4ea830913d7193f08c29239f78d04cdd
Joaquin Quiroga <joaquinquiroga22@gmail.com>
2022-12-08 10:48:36 -0300

    buttonHome

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,13 @@ export default function Home() {
     <div className={styles.container}>
       <h1>Welcome!</h1>
       <Link href="/Post">
-      <button class={styles.learnMore}>
-        <span class={styles.circle} aria-hidden="true">
-          <span class={styles.iconArrow}></span>
-        </span>
-        <span class={styles.buttonText}>Learn More</span>
-      </button>
+        <button className={styles.cta}>
+          <span className={styles.hoverUnderlineAnimation}> Ingresar </span>
+          <svg viewBox="0 0 46 16" height="10" width="30" xmlns="http://www.w3.org/2000/svg" id="arrow-horizontal">
+            <path transform="translate(30)" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" data-name="Path 10" id="Path_10"></path>
+          </svg>
+        </button>
       </Link>
-      </div>
+    </div>
   )
 }
